9ce4571641e602d7d49440c590e1dff6a1b88304
ori <oribarilan@gmail.com>
2023-10-09 12:33:02 +0300

    adjusting benchmark

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -8,21 +8,21 @@ class Person:
     age: int
 
 
-people = [Person('Alice', 28), Person('Bob', 24), Person('Charlie', 29), Person('David', 23)]
+people = [Person(f"Person {i}", i) for i in range(10_000_000)]
 
 
 def using_fliq():
-    return q(people).where(lambda p: p.age > 25).select(lambda p: p.name).first()
+    return q(people).where(lambda p: p.age > 25).select(lambda p: p.name).select(lambda name: name[-1]).first_or_default(default=Person("No one", 0))
 
 
 # Standard library
 def using_standard_lib():
-    return next(map(lambda p: p.name, filter(lambda p: p.age > 25, people)))
+    return next(map(lambda name: name[-1], map(lambda p: p.name, filter(lambda p: p.age > 25, people)))) or Person("No one", 0)
 
 
 # Benchmark
-fliq_time = timeit.timeit(using_fliq, number=1000000)
-std_lib_time = timeit.timeit(using_standard_lib, number=1000000)
+fliq_time = timeit.timeit(using_fliq, number=10000)
+std_lib_time = timeit.timeit(using_standard_lib, number=10000)
 
 print(f"Using fliq: {fliq_time:.5f} seconds")
 print(f"Using standard lib: {std_lib_time:.5f} seconds")
